2a92c8a52fa3983e5c2789de405b8d888dd1d9ad
Sean Killeen <SeanKilleen@gmail.com>
2018-10-29 18:21:07 -0400

    basic tasks with deterministic ordering
    The brackets indicate which tasks must run before this task can run. gulp then determines the best order.

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,19 @@ gulp.task('clean-js', function(){
 
 gulp.task('clean', ['clean-js', 'clean-css']);
 
-gulp.task('css', ['clean-css'], function(){
-
+gulp.task('css', ['sass', 'autoprefixer', 'css-min'], function(){
+  return log('running css');
 });
 
+gulp.task('sass', ['clean-css'], function(){
+  return log('running sass');
+});
+
+gulp.task('autoprefixer', ['sass'], function()
+{
+  return log('running autoprefixer');
+});
+
+gulp.task('css-min', ['autoprefixer'], function(){
+  return log('running css-min');
+});
