064e3d35adfd5f42d005b4b8f1acb4b4facb17f3
Sean Killeen <SeanKilleen@gmail.com>
2018-10-29 22:38:20 -0400

    beginnings of sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require('gulp');
 var log = require('fancy-log');
 var del = require('del');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 var options = {
-
+  sassInputLocationGlob: '_scss/**/*.scss',
+  cssOutputLocation: './_site/css',
+  sassSourcemapsOutputLocation: 'TODO',
+  nodeSassOptions: {
+    // Per the gulp readme at https://github.com/dlmanning/gulp-sass#readme,
+    // These will be passed along directly. Options reference at https://github.com/sass/node-sass#options 
+    outputStyle: 'expanded',
+    includePaths: [
+      'node_modules/uswds/src/stylesheets',
+      'node_modules/uswds/src/stylesheets/lib/'
+    ]
+  }
 }
 
 gulp.task('clean-css', function(){
@@ -21,7 +34,11 @@ gulp.task('css', ['sass', 'autoprefixer', 'css-min'], function(){
 });
 
 gulp.task('sass', ['clean-css'], function(){
-  return log('running sass');
+  return gulp.src(options.sassInputLocationGlob)
+    .pipe(sourcemaps.init())
+    .pipe(sass(options.nodeSassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write(options.sassSourcemapsOutputLocation))
+    .pipe(gulp.dest(options.cssOutputLocation))
 });
 
 gulp.task('autoprefixer', ['sass'], function()
@@ -31,4 +48,8 @@ gulp.task('autoprefixer', ['sass'], function()
 
 gulp.task('css-min', ['autoprefixer'], function(){
   return log('running css-min');
+});
+
+gulp.task('watch:sass', function(){
+  gulp.watch(options.sassInputLocationGlob, ['sass']);
 });
